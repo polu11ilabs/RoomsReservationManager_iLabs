4bc2a296fcaf8550b3d91aa6fe735b7bf307bcad
polu11ilabs <d.polucci@ilabsindustry.it>
2026-09-17 10:22:05 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import VisitorsPage from "./VisitorsPage.jsx";
+import Visitatori from "./Visitatori.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/visitatori" element={<VisitorsPage />} />
+        <Route path="/Visitatori" element={<Visitatori />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
