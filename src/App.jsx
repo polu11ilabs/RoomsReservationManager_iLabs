b63e885b369a44afa775996af0a9524fbaafde95
polu11ilabs <d.polucci@ilabsindustry.it>
2026-09-23 11:41:19 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -793,8 +793,18 @@ function App() {
    */
 
   useEffect(() => {
-    const intervalId = setInterval(() => forceMinuteTick((t) => t + 1), 1000);
-    return () => clearInterval(intervalId);
+    const now = new Date();
+    const msToNextMinute =
+      (60 - now.getSeconds()) * 1000 - now.getMilliseconds();
+    let intervalId = null;
+    const timeoutId = setTimeout(() => {
+      forceMinuteTick((t) => t + 1);
+      intervalId = setInterval(() => forceMinuteTick((t) => t + 1), 60000);
+    }, msToNextMinute);
+    return () => {
+      clearTimeout(timeoutId);
+      if (intervalId) clearInterval(intervalId);
+    };
   }, []);
 
   /*
