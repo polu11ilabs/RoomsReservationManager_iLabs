a945c8d530f911ae11ce55d3e5952a05d13ede39
polu11ilabs <d.polucci@ilabsindustry.it>
2026-09-24 12:01:24 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const root = document.getElementById('root')
-
-try {
-  createRoot(root).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  )
-} catch (e) {
-  console.error('CRASH AL MOUNT:', e)
-  document.body.innerHTML = '<pre style="color:red">' + e.message + '\n' + e.stack + '</pre>'
-}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
